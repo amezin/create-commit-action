3bca0646de309fb04498794fcab593168dbf9f06
Aleksandr Mezin <mezin.alexander@gmail.com>
2025-05-27 01:38:10 +0300

    Include 3rd party component licenses and copyright notices

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 const commonjs = require('@rollup/plugin-commonjs');
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const license = require('rollup-plugin-license');
 
 module.exports = {
     input: 'src/index.js',
@@ -9,5 +10,22 @@ module.exports = {
         sourcemap: true,
         sourcemapExcludeSources: true,
     },
-    plugins: [commonjs(), nodeResolve({ preferBuiltins: true })],
+    plugins: [
+        commonjs(),
+        nodeResolve({ preferBuiltins: true }),
+        license({
+            sourcemap: true,
+            thirdParty: {
+                allow: {
+                    test: '(MIT OR Apache-2.0 OR ISC)',
+                    failOnViolation: true,
+                    failOnUnlicensed: true,
+                },
+                multipleVersions: true,
+                output: {
+                    file: 'dist/dependencies.txt',
+                },
+            },
+        }),
+    ],
 };
